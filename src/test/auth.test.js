915ace80786d2beda1fa27bbbba37d96cb4bd6e0
AshishResolute <anshugourh21@gmail.com>
2026-05-13 14:03:08 +0530

    Tested the signup route

--- src/test/auth.test.js
+++ src/test/auth.test.js
@@ -1,6 +1,19 @@
 import { app } from "../routes/app.js";
 import request from 'supertest'
 import {describe,it} from '@jest/globals'
+
+
+
+beforeAll(async()=>{
+    await request(app)
+    .post('/auth/signUp')
+    .send({email:'test@user.com',password:`Test@user`,confirmPassword:`Test@user`,user_name:`Test User`})
+})
+
+
+
+
+
 describe('AUTH routes',()=>{
 
     describe('POST /auth/signup',()=>{
@@ -12,6 +25,24 @@ describe('AUTH routes',()=>{
 
            expect(res.statusCode).toBe(400);
            expect(res.body).toHaveProperty(`message`)
+        });
+
+        it(`should return 409 if user already exists ie duplicate entry with email`,async()=>{
+           const res = await request(app)
+           .post('/auth/signUp')
+           .send({email:'test@user.com',password:`Test@user`,confirmPassword:`Test@user`,user_name:`Test User`})
+
+           expect(res.statusCode).toBe(409);
+           expect(res.body).toHaveProperty(`message`)
+        });
+
+        it(`Should return 201 for an valid signUp`,async()=>{
+            const res = await request(app)
+            .post('/auth/signUp')
+            .send({email:`test@${Date.now()}.com`,password:`Test@User`,confirmPassword:`Test@User`,user_name:`Test User`})
+
+            expect(res.statusCode).toBe(201)
+            expect(res.body).toHaveProperty(`message`)
         })
     })
 })
